f7ec92f3ab5d087ae2d2f5d3b576de93d013d4e1
Roman Yarosh <yarosh0106@gmail.com>
2025-11-07 18:20:33 +0300

    fixed Class name

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,12 +6,10 @@ import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
 
     @BeforeAll
-    static void beforeAll() {
+    static void configureEnv() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        //Configuration.holdBrowserOpen = true;
-//        Configuration.timeout = 5000; // default 4000
     }
 
 }
